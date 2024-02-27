4498bf8dac36bacb305aad74471209e1b633cd28
ySsio <88093363+ySsio@users.noreply.github.com>
2024-02-27 18:20:12 +0900

    Input System
    - Input Manager을 이용하는 것은 옛날 방식, 유니티에서 input system 새로 제공
    - Player Input 컴포넌트에서 inputaction asset 생성
    - 제공해주는 다양한 함수 사용 가능 (OnMove()로 입력 벡터를 쉽게 받아서 사용했음)

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -18,13 +19,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Input.GetAxis()를 통해 좌우/상하 입력 받을 수  있음. -> project setting
-        // Input.GetAxis()에는 보간법이 적용되어 키보드에서 손을 떼도 미끄러지는 현상 발생. Input.GetAxisRaw()는 -1/0/1 이렇게 값을 딱딱 끊어주므로 이런 현상 없앨 수 있음.
-
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+        // ## Input Manager을 이용한 방식
+        //// Input.GetAxis()를 통해 좌우/상하 입력 받을 수  있음. -> project setting
+        //// Input.GetAxis()에는 보간법이 적용되어 키보드에서 손을 떼도 미끄러지는 현상 발생. Input.GetAxisRaw()는 -1/0/1 이렇게 값을 딱딱 끊어주므로 이런 현상 없앨 수 있음.
+        //inputVec.x = Input.GetAxisRaw("Horizontal");
+        //inputVec.y = Input.GetAxisRaw("Vertical");
         
-        Move();
     }
 
     void FixedUpdate()
@@ -43,8 +43,10 @@ public class Player : MonoBehaviour
         // 사용자의 프레임마다 update/fixedupdate 주기가 다르기 때문에 Time.deltaTime/Time.fixedDeltaTime을 곱해주지 않으면 환경에 따라 차이가 발생함.
     }
 
-    private void Move()
+
+    // ## Input System을 이용한 방식
+    private void OnMove(InputValue _inputValue)
     {
-        
+        inputVec = _inputValue.Get<Vector2>();  // 이미 normalize를 사용하기로 세팅했으므로 위에 FixedUpdate 문에서 normalized 사용 안해도 됨.
     }
 }
