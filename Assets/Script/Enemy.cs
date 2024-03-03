fe6d41888dcecf2e8324226b630e8c1fbce63631
ySsio <88093363+ySsio@users.noreply.github.com>
2024-03-03 09:45:13 +0900

    Enemy (4) - hit, knockback
    - 애니메이터 타임라인에서 event 추가해서 시간에 따라 함수 실행시킬 수 있음! 이거로 죽였을 때 사망 애니메이션 1초동안 실행된 후 Dead()함수 호출하도록 함.
    - 인스펙터 헤더 사용
    - 코루틴, WaitForFixedUpdate로 물리 1프레임 쉰다는데 무슨 의미인지 모르겠음

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -6,36 +6,52 @@ using UnityEngine.InputSystem.Processors;
 
 public class Enemy : MonoBehaviour
 {
+    // status
     public float speed;
     public float health;    // 현재 체력
     public float maxHealth; // 최대 체력
-    public RuntimeAnimatorController[] animCon;
-    public Rigidbody2D target;
-
+    
+    // isAlive
     public bool isAlive;
 
-    private Rigidbody2D rigid;
-    private SpriteRenderer spriteRenderer;
+    // target
+    public Rigidbody2D target;
+
+    // Components - Animation,Sprite
+    public RuntimeAnimatorController[] animCon;
     private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    // Components - Physics
+    private Rigidbody2D rigid;
+    private Collider2D coll;
+    
+    
+    // 물리 1 프레임 시간?
+    WaitForFixedUpdate wait;
+
+    // this to target direction
+    Vector2 dirVec;
 
 
     void Awake()
     {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = Vector2.zero;
-
-        if (!isAlive)
+        if (!isAlive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
             return;
 
+        rigid.velocity = Vector2.zero;
 
-        Vector2 dirVec = (target.position - rigid.position).normalized;
+        dirVec = (target.position - rigid.position).normalized;
         Vector2 nextPos = rigid.position + dirVec * speed * Time.fixedDeltaTime;
 
         rigid.MovePosition(nextPos);    // 프레임마다 순간이동 하는 이동 방식
@@ -58,6 +74,10 @@ public class Enemy : MonoBehaviour
     private void OnEnable()
     {
         isAlive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        anim.SetBool("Dead", false);
+        spriteRenderer.sortingOrder = 2;
         health = maxHealth;
     }
 
@@ -66,28 +86,43 @@ public class Enemy : MonoBehaviour
         anim.runtimeAnimatorController = animCon[_spawndata.spriteType];
         speed = _spawndata.speed;
         maxHealth = _spawndata.maxHealth;
+        health = maxHealth;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (!collision.CompareTag("Bullet"))
+        if (!collision.CompareTag("Bullet") || !isAlive)
             return;
 
         // Bullet을 맞으면 그 bullet의 데미지만큼 현재 체력을 감소시킴
         health -= collision.GetComponent<Bullet>().damage;
+        
         if (health > 0)
         {
             // .. 살아있는 경우.. 히트 액션 등
+            
+            anim.SetTrigger("Hit");
+            StartCoroutine(KnockBack());
         }
         else
         {
             // .. 죽은 경우 안 맞음
-            Dead();
+            isAlive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            anim.SetBool("Dead", true);
+            spriteRenderer.sortingOrder = 1;
+            GameManager.instance.kills++;
+            GameManager.instance.GetExp();
         }
-
-
-
     }
+
+    IEnumerator KnockBack()
+    {
+        yield return wait;
+        rigid.AddForce(-dirVec * 3, ForceMode2D.Impulse);
+    }
+
 
     // 사망
     void Dead()
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,10 +6,22 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [Header("# Game Control")]
     public float gameTime; // 게임시간
     public float maxGameTime = 20f;
+    
+    [Header("# Player Info")]
+    public int level;
+    public int kills;
+    public int exp;
+    public int[] MaxExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
+
+    [Header("# Game Object")]
     public Player player;
     public PoolManager poolManager;
+
+
+
     private void Awake()
     {
         instance = this;
@@ -22,6 +34,17 @@ public class GameManager : MonoBehaviour
         if (gameTime >= maxGameTime)
         {
             gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp()
+    {
+        ++exp;
+
+        if (exp >= MaxExp[level])
+        {
+            ++level;
+            exp = 0;
         }
     }
 }
